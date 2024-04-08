37f3bc51f9fa118b5cd5848ec700887ff537579e
Allen Chrisso Dhas <96862234+allencdhas@users.noreply.github.com>
2024-04-08 15:35:21 +0530

    --

--- Naanmudhalvan_Raj_kumar_5046_LICET/media_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.indexPage),
-    path('allsongs/',views.allsongs),
-]
--- Naanmudhalvan_Raj_kumar_5046_LICET/user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register',views.new_register,name="register"),
-    path('login',views.new_login,name="login"),
-    path('logout',views.logout,name="logout"),
-
-]
